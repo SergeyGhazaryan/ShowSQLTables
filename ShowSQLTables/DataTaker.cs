07e90b8e48229a16ec1eb14743a0b654150ba899
Seg <sergey.ghazaryan2000@gmail.com>
2021-01-13 23:04:15 -0800

    Changed Shitikyan comments

---
+++ ShowSQLTables/DataTaker.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace ShowSQLTables
+{
+    class DataTable
+    {
+        private SqlCommand command;
+        private SqlDataReader reader;
+
+        public DataTable(SqlCommand command, SqlDataReader reader)
+        {
+            this.command = command;
+            this.reader = reader;
+        }
+
+        public void DataTaker()
+        {
+            if (reader.HasRows)
+            {
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    Console.Write("{0} \t", reader.GetName(i));
+                }
+                Console.WriteLine();
+
+                while (reader.Read())
+                {
+                    int num = 0;
+                    while (num < reader.FieldCount)
+                    {
+                        object property = reader.GetValue(num);
+
+                        Console.Write("{0} \t", property);
+
+                        num++;
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
--- ShowSQLTables/Program.cs
+++ ShowSQLTables/Program.cs
@@ -13,40 +13,18 @@ namespace ShowSQLTables
             {
                 connection.Open();
                 SqlCommand command1 = new SqlCommand("SELECT * FROM Author", connection);
-                SqlCommand command2 = new SqlCommand("SELECT * FROM Book", connection);
                 SqlDataReader reader1 = command1.ExecuteReader();
+
+                DataTable dataTable1 = new DataTable(command1, reader1);
+                dataTable1.DataTaker();
+
+                Console.WriteLine();
+
+                SqlCommand command2 = new SqlCommand("SELECT * FROM Book", connection);
                 SqlDataReader reader2 = command2.ExecuteReader();
 
-                if (reader1.HasRows || reader2.HasRows)
-                {
-                    Console.WriteLine("{0} \t{1} \t{2} \t{3} \t{4}", reader1.GetName(0), reader1.GetName(1), reader1.GetName(2), reader1.GetName(3), reader1.GetName(4));
-                    while (reader1.Read())
-                    {
-                        object authorId = reader1.GetValue(0);
-                        object firstName = reader1.GetValue(1);
-                        object lastName = reader1.GetValue(2);
-                        object age = reader1.GetValue(3);
-                        object country = reader1.GetValue(4);
-
-                        Console.WriteLine("{0} \t{1} \t{2} \t{3} \t{4}", authorId, firstName, lastName, age, country);
-                    }
-                    Console.WriteLine();
-
-                    Console.WriteLine("{0} \t{1} \t{2} \t{3}", reader2.GetName(0), reader2.GetName(1), reader2.GetName(2), reader2.GetName(3));
-                    while (reader2.Read())
-                    {
-                        object bookId = reader2.GetValue(0);
-                        object name = reader2.GetValue(1);
-                        object date = reader2.GetValue(2);
-                        object bAuthorId = reader2.GetValue(3);
-
-                        Console.WriteLine("{0} \t{1} \t{2} \t{3}", bookId, name, date, bAuthorId);
-                    }
-                }
-                else
-                {
-                    Console.WriteLine("Do you have a invalide table.");
-                }
+                DataTable dataTable2 = new DataTable(command2, reader2);
+                dataTable2.DataTaker();
             }
         }
     }
